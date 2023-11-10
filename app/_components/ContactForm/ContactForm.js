7c021b41093b2be0cfd7e08cb2e501adec0d3b7c
DBHaslund <me@bzyhas.com>
2023-11-10 13:05:22 +0100

    Added dialog box to contact form for sent mail.

--- app/_components/ContactForm/ContactForm.js
+++ app/_components/ContactForm/ContactForm.js
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { AiOutlineLoading } from 'react-icons/ai';
 import sendMail from '../../_utils/sendMail';
 import validateRecaptcha from '../../_utils/validateRecaptcha';
 import Captcha from 'react-google-recaptcha';
@@ -18,6 +19,7 @@ const ContactForm = () => {
     isSuccessful: false,
     message: '',
   });
+  const [mailSent, setMailSent] = useState(false);
 
   const recaptchaRef = useRef();
 
@@ -57,84 +59,95 @@ const ContactForm = () => {
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
       reset();
+      setMailSent(true);
     }
-  });
+  }, [formState.isSubmitSuccessful, reset]);
 
   const onError = (errors) => console.log(errors);
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit, onError)}
-      className='mx-auto my-8 text-xl p-2 bg-gray rounded-lg flex-col'
-    >
-      <div className='w-4/5 max-w-md mx-auto py-4'>
-        <label htmlFor='name' className=''>
-          {errors.name?.message || 'Name'}
-        </label>
-        <input
-          type='text'
-          id='name'
-          {...register('name', { required: 'Please enter your name.' })}
-          autoComplete='name'
-          placeholder='Name'
-          className='w-full px-4 py-2 mb-2 mt-1 rounded drop-shadow bg-dgray placeholder:opacity-40'
-        />
-        <label htmlFor='email' className=''>
-          {errors.email?.message || 'Email'}
-        </label>
-        <input
-          type='email'
-          id='email'
-          {...register('email', { required: 'Please enter a valid email.' })}
-          autoComplete='email'
-          placeholder='Email address'
-          className='w-full px-4 py-2 mb-2 mt-1 rounded drop-shadow bg-dgray placeholder:opacity-40'
-        />
-        <label htmlFor='message' className=''>
-          {errors.message?.message || 'Message'}
-        </label>
-        <textarea
-          rows='5'
-          id='message'
-          {...register('message', { required: 'Please include a message.' })}
-          placeholder='Write your message...'
-          className='w-full px-4 py-2 mb-2 mt-1 rounded drop-shadow bg-dgray placeholder:opacity-40'
-        />
-        <div className='flex'>
-          <p className='w-62 text-xs w-2/3'>
-            This site is protected by reCAPTCHA and the Google
-            <a
-              className='underline hover:text-white m-1'
-              href='https://policies.google.com/privacy'
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit, onError)}
+        className='relative mx-auto my-8 text-xl p-2 bg-gray rounded-lg flex-col'
+      >
+        <div className='w-4/5 max-w-md mx-auto py-4'>
+          <label htmlFor='name'>{errors.name?.message || 'Name'}</label>
+          <input
+            type='text'
+            id='name'
+            {...register('name', { required: 'Please enter your name.' })}
+            autoComplete='name'
+            placeholder='Name'
+            className='w-full px-4 py-2 mb-2 mt-1 rounded drop-shadow bg-dgray placeholder:opacity-40'
+          />
+          <label htmlFor='email'>{errors.email?.message || 'Email'}</label>
+          <input
+            type='email'
+            id='email'
+            {...register('email', { required: 'Please enter a valid email.' })}
+            autoComplete='email'
+            placeholder='Email address'
+            className='w-full px-4 py-2 mb-2 mt-1 rounded drop-shadow bg-dgray placeholder:opacity-40'
+          />
+          <label htmlFor='message'>
+            {errors.message?.message || 'Message'}
+          </label>
+          <textarea
+            rows='5'
+            id='message'
+            {...register('message', { required: 'Please include a message.' })}
+            placeholder='Write your message...'
+            className='w-full px-4 py-2 mb-2 mt-1 rounded drop-shadow bg-dgray placeholder:opacity-40'
+          />
+          <div className='flex'>
+            <p className='w-62 text-xs w-2/3'>
+              This site is protected by reCAPTCHA and the Google
+              <a
+                className='underline hover:text-white m-1'
+                href='https://policies.google.com/privacy'
+              >
+                Privacy Policy
+              </a>{' '}
+              and
+              <a
+                className='underline hover:text-white m-1'
+                href='https://policies.google.com/terms'
+              >
+                Terms of Service
+              </a>{' '}
+              apply.
+            </p>
+            {formState.isSubmitting && (
+              <AiOutlineLoading
+                size={'2rem'}
+                className='animate-spin ml-auto my-1'
+              />
+            )}
+            <button
+              type='submit'
+              className='rounded drop-shadow py-2 px-4 bg-dgray block ml-auto hover:text-white'
             >
-              Privacy Policy
-            </a>{' '}
-            and
-            <a
-              className='underline hover:text-white m-1'
-              href='https://policies.google.com/terms'
-            >
-              Terms of Service
-            </a>{' '}
-            apply.
-          </p>
-          <button
-            type='submit'
-            className='rounded drop-shadow py-2 px-4 bg-dgray block ml-auto hover:text-white'
-          >
-            Send
-          </button>
+              Send
+            </button>
+          </div>
         </div>
-      </div>
-      <div className='flex justify-center'>
-        <Captcha
-          ref={recaptchaRef}
-          size='invisible'
-          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-          theme='dark'
-        />
-      </div>
-    </form>
+        <div className='flex justify-center'>
+          <Captcha
+            ref={recaptchaRef}
+            size='invisible'
+            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+            theme='dark'
+          />
+        </div>
+      {mailSent && (
+          <div className='absolute h-40 w-64 p-8 mx-auto inset-x-0 inset-y-12 bg-gray shadow-2xl border text-center rounded-lg'>
+            <h3>Thank you for your message!</h3>
+            <button onClick={() => setMailSent(false)} className='border m-auto px-2 pb-1 mt-2 hover:opacity-80'>Close</button>
+          </div>
+      )}
+      </form>
+    </>
   );
 };
 
