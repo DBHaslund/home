6a48288c51fd41811e67d7f417435520b9f0048e
unknown <me@bzyhas.com>
2024-09-10 15:25:00 +0200

    Css tweaks

--- app/_components/Sections/About.js
+++ app/_components/Sections/About.js
@@ -8,19 +8,19 @@ const About = () => {
     <Section id='about'>
       <h2 className='text-center text-3xl font-semibold'>About Me</h2>
       <div className='pt-4 px-16 md:px-32'>
-        <p className='mb-8'>
+        <p className='mb-8 '>
           After a lifelong interest in tech, dabbling with various things over
           the years, a good friend of mine finally pushed me to actually learn
           something concrete a couple of years ago. It started with simple
           CSS/JS, and since then I&apos;ve delved into ReactJS, NextJS,
           Tailwind, as well as a bit of Wordpress.
         </p>
-        <div className='items-center'>
-          <p className='text-center'>
-            I&apos;m always trying to improve my skills. I&apos;m currently
-            relatively comfortable with:
-          </p>
-          <ul className=''>
+        <p className='text-center'>
+          I&apos;m always trying to improve my skills. I&apos;m currently
+          relatively comfortable with:
+        </p>
+        <div className='flex justify-center pt-4'>
+          <ul>
             <li className='flex p-1'>
               <i className='text-icon'>
                 <TiHtml5 />
